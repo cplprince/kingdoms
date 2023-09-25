24bc05c545aac894df58de5e8ed2e3ff5f95b338
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-09-25 15:16:00 +0200

    fix Sibebar

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
 import { Sidebar } from "@/components/sidebar/Sidebar";
-import { images } from "../assets/";
 
 export default function Home() {
   return (
     <main>
-      {images.map((image, index) => (
-        <Sidebar
-          src={image.src}
-          alt={image.alt}
-          key={`${image.alt}-${index}`}
-        />
-      ))}
+      <Sidebar />
     </main>
   );
 }
--- FE/src/components/sidebar/Sidebar.tsx
+++ FE/src/components/sidebar/Sidebar.tsx
@@ -3,30 +3,27 @@ import Image from "next/image";
 
 import { cn } from "../../../lib/utils";
 import styles from "./Components.module.css";
+import { images } from "../../assets";
 
-type Props = {
-  src: string;
-  alt: string;
-};
-
-export const Sidebar: FC<Props> = ({ src, alt }) => {
+export const Sidebar: FC = () => {
   const sidebarContent = cn("bg-primary-second p-5");
-  const sidebarDivImg = cn(styles.sidebarDivImg);
-  const sidebarContainer = cn(styles.sidebarContainer);
-  const sidebarImg = cn(styles.sidebarImg);
 
   return (
-    <div className={sidebarContainer}>
+    <div className={styles.sidebarContainer}>
       <div className={sidebarContent}>
-        <div onClick={() => console.log(alt)} className={sidebarDivImg}>
-          <Image
-            src={src}
-            alt={alt}
-            width={100}
-            height={100}
-            className={sidebarImg}
-          />
-        </div>
+        {images.map((image, index) => {
+          return (
+            <div className={styles.sidebarDivImg} key={`${image.alt}-${index}`}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={100}
+                height={100}
+                className={styles.sidebarImg}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
