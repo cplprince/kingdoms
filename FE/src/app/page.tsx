8acb3f3362282e41b39f4cfc822d3580ae4ad96c
Patrik Ludvik <99814207+Patrik93JS@users.noreply.github.com>
2023-09-26 08:05:35 +0200

    sidebar brasko (#2)
    * sidebar brasko
    
    * fix Sibebar
    
    * uprava classy

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Sidebar } from "@/components/sidebar/Sidebar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Ahoj Main page</div>
+    <main>
+      <Sidebar />
     </main>
   );
 }
---
+++ FE/src/assets/index.ts
@@ -0,0 +1,11 @@
+import tree from './tree-silhouette.svg'
+import corn from './corn.webp'
+
+export const images = [{
+    src: tree,
+    alt: "tree"
+},
+{
+    src: corn,
+    alt: 'corn'
+}]
