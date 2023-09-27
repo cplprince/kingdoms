5ec6df8d6c7e76b977fa5968d01ad38387d37cda
CplPrince <endrych@centrum.cz>
2023-09-27 14:05:33 +0200

    test (#5)
    Co-authored-by: Tomáš Endrych <tomas.endrych@superkoders.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import { Providers } from "@/store/providers";
-import "./globals.css";
+import { Providers } from '../../lib/providers';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <Providers>
-      <html lang="en">
-        <body className={"bg-black"}>{children}</body>
-      </html>
-    </Providers>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<Providers>
+			<html lang="en">
+				<body className={'bg-black'}>{children}</body>
+			</html>
+		</Providers>
+	);
 }
