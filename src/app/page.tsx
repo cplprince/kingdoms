33c6e8b3cd68fbe66208ebcb313f7c934f301ff9
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-10-02 13:29:19 +0200

    inventar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-"use client";
+'use client';
 
-import { Sidebar } from "@/components/sidebar/Sidebar";
+import { Inventory } from '@/components/inventory/Inventory';
+import { Sidebar } from '@/components/sidebar/Sidebar';
+import { useModal } from '../hooks/useModal';
+import { cn } from '../../lib/utils';
 
 export default function Home() {
-  return (
-    <main>
-      <Sidebar />
-    </main>
-  );
+	const { isOpen, toggle } = useModal();
+	const inventoryContainer = cn('container mx-auto mt-4 absolute pl-32');
+	const buttonInventory = cn('text-2xl font-bold border-4 p-4 mb-4 border-primary-first text-primary-third');
+
+	const items = [];
+	for (let i = 1; i <= 50; i++) {
+		items.push({ id: i, name: `Item ${i}` });
+	}
+
+	return (
+		<main>
+			<Sidebar />
+			<div className={inventoryContainer}>
+				<button className={buttonInventory} onClick={toggle}>
+					Inventory
+				</button>
+				{isOpen && <Inventory items={items} />}
+			</div>
+		</main>
+	);
 }
--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { FC } from 'react';
+import styles from './Components.module.css';
 
-export const Inventory = () => {
-	return <div>Inventory</div>;
+type InventoryItem = {
+	id: number;
+	name: string;
+};
+
+type Props = {
+	items: InventoryItem[];
+};
+
+export const Inventory: FC<Props> = ({ items }) => {
+	return (
+		<div className={styles.inventoryContainer}>
+			<div className={styles.inventoryContent}>
+				{items.map((item) => (
+					<div key={item.id} className={styles.inventoryItem}>
+						{item.name}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
